f811cd8dca0c5ea46afe3ee6f3b7f62a9d69a812
ascete <aleger12@yandex.ru>
2021-08-18 23:14:31 +0500

    Исправил: 1.в дао при каждом запросе ты создаешь новый коннекшн, реализуй так, что бы он был один 2. в сервисе при каждом запросе ты создаешь новый дао. это излишне. сделай один дао

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,93 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
-public class UserDaoJDBCImpl implements UserDao {
+public class UserDaoJDBCImpl extends Util implements UserDao {
+
+    Connection connectUtil = getConnection();
+
     public UserDaoJDBCImpl() {
 
     }
 
+    @Override
     public void createUsersTable() {
-
+        try (Statement st1 = connectUtil.createStatement()) {
+                st1.execute("create table if not exists Users (" +
+                    "ID bigint primary key auto_increment," +
+                    "Name varchar(15) not null," +
+                    "LastName varchar(15)," +
+                    "Age tinyint)");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
+    @Override
     public void dropUsersTable() {
-
+        try (Statement st1 = connectUtil.createStatement()) {
+            st1.execute("drop table if exists Users");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void saveUser(String name, String lastName, byte age) {
-
+    @Override
+    public void saveUser(String name, String lastName, int age) {
+        String sql = "insert into Users (Name, LastName, Age) values (?, ?, ?)";
+        try (PreparedStatement st1 = connectUtil.prepareStatement(sql)) {
+            st1.setString(1, name);
+            st1.setString(2, lastName);
+            st1.setByte(3, (byte) age);
+            st1.executeUpdate();
+            System.out.println("User с именем – " + name + " добавлен в базу данных");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
+    @Override
     public void removeUserById(long id) {
-
+        String sql = "delete from Users where ID = ?";
+        try (PreparedStatement st1 = connectUtil.prepareStatement(sql)) {
+            st1.setLong(1, id);
+            st1.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
+    @Override
     public List<User> getAllUsers() {
-        return null;
+        List<User> list = new ArrayList<>();
+        try (Statement st1 = connectUtil.createStatement()) {
+            ResultSet resultSet = st1.executeQuery("select * from Users");
+            while (resultSet.next()) {
+                User tUser = new User();
+                tUser.setId(resultSet.getLong("ID"));
+                tUser.setName(resultSet.getString("Name"));
+                tUser.setLastName(resultSet.getString("LastName"));
+                tUser.setAge(resultSet.getByte("Age"));
+                list.add(tUser);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return list;
     }
 
+    @Override
     public void cleanUsersTable() {
-
+        try (Statement st1 = connectUtil.createStatement()) {
+            st1.execute("truncate table Users");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,31 +1,40 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
+    private UserDao userDaoImp = new UserDaoJDBCImpl();
+    @Override
     public void createUsersTable() {
-
+        userDaoImp.createUsersTable();
     }
 
+    @Override
     public void dropUsersTable() {
-
+        userDaoImp.dropUsersTable();
     }
 
-    public void saveUser(String name, String lastName, byte age) {
-
+    @Override
+    public void saveUser(String name, String lastName, int age) {
+        userDaoImp.saveUser(name, lastName, age);
     }
 
+    @Override
     public void removeUserById(long id) {
-
+        userDaoImp.removeUserById(id);
     }
 
+    @Override
     public List<User> getAllUsers() {
-        return null;
+        return userDaoImp.getAllUsers();
     }
 
+    @Override
     public void cleanUsersTable() {
-
+        userDaoImp.cleanUsersTable();
     }
 }
