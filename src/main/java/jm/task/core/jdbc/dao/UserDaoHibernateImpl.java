b8101af50afa55fc1489ea3db0cd3afe88bd4bec
aleger12@yandex.ru <Aleger19791989>
2021-08-23 13:57:17 +0500

    пароль 123

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,42 +1,94 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
-public class UserDaoHibernateImpl implements UserDao {
-    public UserDaoHibernateImpl() {
 
+
+public class UserDaoHibernateImpl extends Util implements UserDao {
+
+    public UserDaoHibernateImpl() {
     }
+
 
 
     @Override
     public void createUsersTable() {
-
+        try(Session s = getSessionFactory().openSession()) {
+            s.beginTransaction();
+            s.createSQLQuery("create table if not exists Users (" +
+                    "ID bigint primary key auto_increment," +
+                    "Name varchar(15) not null," +
+                    "LastName varchar(15)," +
+                    "Age tinyint)").executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void dropUsersTable() {
-
+        try(Session s = getSessionFactory().openSession()) {
+            s.beginTransaction();
+            s.createSQLQuery("drop table if exists Users").executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
-    public void saveUser(String name, String lastName, int age) {
-
+    public void saveUser(String name, String lastName, byte age) {
+        Session session = getSessionFactory().openSession();
+        session.beginTransaction();
+        session.save(new User(name, lastName, age));
+        session.getTransaction().commit();
+        session.close();
     }
 
     @Override
     public void removeUserById(long id) {
-
+        try (Session s = getSessionFactory().openSession()){
+            s.beginTransaction();
+            User u = new User();
+            u.setId(id);
+            s.delete(u);
+            s.getTransaction().commit();
+            System.out.println("User с ID – " + id + " удалён");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        List<User> list = new ArrayList<>();
+        try (Session s = getSessionFactory().openSession()){
+            list= s.createQuery("SELECT a FROM User a", User.class).getResultList();
+            System.out.println(list);
+            return list;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
     }
 
     @Override
     public void cleanUsersTable() {
-
+        try (Session s = getSessionFactory().openSession()){
+            s.beginTransaction();
+            s.createQuery("delete User").executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
 }
--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -1,13 +1,14 @@
 package jm.task.core.jdbc.model;
 
 
-import javax.persistence.Column;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.Objects;
 
-@Table
+@Entity
+@Table(name = "User")
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column
@@ -59,5 +60,27 @@ public class User {
 
     public void setAge(Byte age) {
         this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(name, user.name) && Objects.equals(lastName, user.lastName) && Objects.equals(age, user.age);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, lastName, age);
     }
 }
